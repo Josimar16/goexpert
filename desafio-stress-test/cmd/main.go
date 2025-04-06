03a17d6dcef1127e23fc8a3a6c7db8776659fd43
Josimar16 <josimarjr479@gmail.com>
2025-04-06 12:26:10 -0300

    adicionando desafio do stress test

---
+++ desafio-stress-test/cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/josimar16/goexpert/desafio-stress-test/internal/infra/cli"
+)
+
+func main() {
+	err := cli.RootCmd.Execute()
+	if err != nil {
+		fmt.Printf("Fail to execute root cmd: %v", err)
+		os.Exit(1)
+	}
+}
